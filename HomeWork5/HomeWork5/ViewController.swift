ec1df39de0bae3e610d465037b4ab5026de1c1bb
Avrama16 <147166187+Avrama16@users.noreply.github.com>
2024-03-13 16:14:10 +0200

    [add] homework done

--- HomeWork5/HomeWork5/ViewController.swift
+++ HomeWork5/HomeWork5/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var enterButton: UIButton!
+    
+    @IBOutlet weak var regButton: UIButton!
+    
+    @IBOutlet weak var emailType: UITextField!
+    
+    @IBOutlet weak var passwordType: UITextField!
+    
+    @IBOutlet weak var pasLable: UILabel!
+    
+    @IBOutlet weak var emailLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func enterButtonAction(_ sender: Any) {
+        print("Did tap")
+    }
+    @IBAction func regButtonAction(_ sender: Any) {
+        print("Did tap")
+    }
+    
 }
 
